d4bb968817ceaa6b1d30581676019dc17ca39000
Hamidreza Sadr <hamidrezasadr610@gmail.com>
2024-11-10 20:03:29 +0330

    add Datasource

---
+++ src/main/java/repository/Datasource.java
@@ -0,0 +1,38 @@
+package repository;
+
+import lombok.Getter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Datasource {
+
+    @Getter
+    private static final Connection connection;
+
+    private Datasource() {
+    }
+
+    static {
+        final String DB_URL ="jdbc:postgresql://localhost:5432/twiiter";
+        final String USER = "postgres";
+        final String PASS = "postgres";
+        try {
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        // JVM Shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+}
