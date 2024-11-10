f55201e06f6417baf8ad827bdad3f829e82ba6e7
Hamidreza Sadr <hamidrezasadr610@gmail.com>
2024-11-11 00:16:21 +0330

    delete update method

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -6,6 +6,5 @@ import java.sql.SQLException;
 
 public interface UserRepository {
     User create(User user) throws SQLException;
-    User read(int id)  throws SQLException;
-    User update(User user) throws SQLException;
+    User read(long id)  throws SQLException;
 }
